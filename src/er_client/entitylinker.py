c027b219846fb283a9edb221062cfaf73d4a6d2c
kkpathak91 <39095706+kkpathak91@users.noreply.github.com>
2022-08-30 00:46:59 +0530

    Update entitylinker.py

--- src/er_client/entitylinker.py
+++ src/er_client/entitylinker.py
@@ -30,7 +30,7 @@ class ELClient:
         self.link_type = link_type
         if link_type == 'tagme':
             self.min_rho = min_rho
-            tagme.GCUBE_TOKEN = os.system('export TAGME_APIKEY="f0dac4d5-7079-4ffe-be98-9ebbccfd5b3a-843339462"')
+            tagme.GCUBE_TOKEN = "f0dac4d5-7079-4ffe-be98-9ebbccfd5b3a-843339462"
         elif link_type == 'spacy':
             assert prefix is not None
             self.init_spacy_linker(prefix)
